18f1edb4f8d6840926e542ef24ff1f6574b0a620
Caio Leonardi <caio.leonardi@titotal.com>
2017-07-19 14:57:43 -0300

    - Adicionado webservice

---
+++ LoggerWcf/ILoggerWriter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LoggerWcf
+{
+    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da interface "IService1" no arquivo de código e configuração ao mesmo tempo.
+    [ServiceContract]
+    public interface ILoggerWriter
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/Escrever")]
+        bool FazerLog<T>(T DadosLog, dynamic SegundoDados, int EnumTipoLog);
+
+        // TODO: Adicione suas operações de serviço aqui
+    }
+
+
+    // Use um contrato de dados como ilustrado no exemplo abaixo para adicionar tipos compostos a operações de serviço.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
